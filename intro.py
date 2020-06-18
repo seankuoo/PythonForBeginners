d5b5269e6534682e62b2984c5279adb51781af63
candidpinenut <seankuoo@gmail.com>
2020-06-18 12:33:13 +0800

    append

---
+++ intro.py
@@ -0,0 +1,31 @@
+
+# import my_moudle as mm 
+# from my_moudle import find_index, test
+# from my_moudle import find_index as fi, test
+from my_moudle import *
+import sys
+import random
+import math
+import datetime
+import calendar
+import os
+
+# courses = ['History', 'Math', 'Physics', 'CompSci']
+
+# index = mm.find_index(courses, 'Math')
+# index = find_index(courses, 'Math')
+# index = find_index (courses, 'Math')
+# random_courses = random.choice(courses)
+# rads = math.radians(90)
+# toady = datetime.date.today()
+
+# print(index)
+# print(test)
+
+# print(random_courses)
+# print(rads)
+# print(math.sin(rads))
+# print(toady)
+# print(calendar.isleap(2020)
+print(os.getcwd())
+print(os.__file__)
